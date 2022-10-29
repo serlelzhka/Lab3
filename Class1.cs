343bce026965c00025fc7657318ba07256ddf4f4
serlelzhka <sergeilyashenko333@bk.ru>
2022-10-29 18:47:22 +0800

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Square
+    {
+        private double value;
+        private string type;
+        public Square(double value, string type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+        public string Verbose()
+        {
+            return String.Format("{0} {1}", this.value, this.type);
+        }
+    }
+}
