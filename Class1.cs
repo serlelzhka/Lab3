508aeb0f7da859f0bc8b92fc7b7c0079b1d4d581
serlelzhka <sergeilyashenko333@bk.ru>
2022-10-29 21:19:43 +0800

    Базовые возможности

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lab3
 {
-    public enum MeasureType { mm, ga, ap, des };
+    public enum MeasureType { mm, ga, ar, des };
 
     public class Square
     {
@@ -28,14 +28,89 @@ namespace Lab3
                 case MeasureType.ga:
                     typeVerbose = "га";
                     break;
-                case MeasureType.ap:
-                    typeVerbose = "ап";
+                case MeasureType.ar:
+                    typeVerbose = "ар";
                     break;
                 case MeasureType.des:
                     typeVerbose = "десятин";
                     break;
             }
             return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+        public static Square operator +(Square instance, double number)
+        {
+            return new Square(instance.value + number, instance.type);
+        }
+        public static Square operator +(double number, Square instance)
+        {
+            return instance + number;
+        }
+        public static Square operator -(Square instance, double number)
+        {
+            return new Square(instance.value - number, instance.type);
+        }
+        public static Square operator -(double number, Square instance)
+        {
+            return instance - number;
+        }
+        public static Square operator *(Square instance, double number)
+        {
+            return new Square(instance.value * number, instance.type);
+        }
+        public static Square operator *(double number, Square instance)
+        {
+            return instance * number;
+        }
+        public Square To(MeasureType newType)
+        {
+            var newValue = this.value;
+            
+            if (this.type == MeasureType.mm)
+            {
+                
+                switch (newType)
+                {
+                    
+                    case MeasureType.mm:
+                        newValue = this.value;
+                        break;
+                    
+                    case MeasureType.ga:
+                        newValue = this.value / 10000;
+                        break;
+                    
+                    case MeasureType.ar:
+                        newValue = this.value / 100;
+                        break;
+                    
+                    case MeasureType.des:
+                        newValue = this.value / 10925.4;
+                        break;
+                }
+            }
+            else if (newType == MeasureType.mm)
+            {
+                switch (this.type)
+                {
+                    case MeasureType.mm:
+                        newValue = this.value;
+                        break;
+                    case MeasureType.ga:
+                        newValue = this.value * 10000;
+                        break;
+                    case MeasureType.ar:
+                        newValue = this.value * 100;
+                        break;
+                    case MeasureType.des:
+                        newValue = this.value * 10925.4;
+                        break;
+                }
+            }
+            else
+            {
+                newValue = this.To(MeasureType.mm).To(newType).value;
+            }
+            return new Square(newValue, newType);
         }
     }
 }
